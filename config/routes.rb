253ad4cc5f4cde1221e9b3de280a269cd5cea11a
Ubuntu <ubuntu@ip-172-31-20-186.ap-southeast-1.compute.internal>
2022-10-03 03:24:41 +0000

    rails g controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#landing_page"
+  get 'static_pages/landing_page'
+  get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "articles#index"
 end
