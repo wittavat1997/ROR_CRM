3daa39e9a65324274adbf3788b8ef920c872cc48
Wittavat <Wittavat.s@ku.th>
2022-10-16 05:22:39 +0000

    create courses table

---
+++ db/migrate/20221016052054_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :courses do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :classroom, null: false, foreign_key: true
+      t.references :serives, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
