ff8f095f5c2be0c2e8a5b64378bf492f66465070
Wittavat <Wittavat.s@ku.th>
2022-10-16 04:07:08 +0000

    create services and classroom table

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.1"
 
-
+gem 'simple_form'
 gem 'invisible_captcha'
 gem 'devise_invitable', '~> 2.0.0'
 gem "omniauth", "~> 1.9.1" #fix Not found. Authentication passthru by version 1.9.1.
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_024433) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_040446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.integer "client_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
